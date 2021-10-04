9bac308c91e364ec2cd67d78ec3d41bbcda791f4
GooseMan0001 <82244189+GooseMan0001@users.noreply.github.com>
2021-10-04 15:39:35 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'All the cheaters will cry about getting banned from your FiveM Server after installing Cry-Anticheat.'
+name 'Cry-Anticheat'
+author 'Crypto'
+contact 'Crypto#0003'
+
+version '2.0.0'
+
+server_only 'yes'
+
+server_scripts {
+          '',
+}
